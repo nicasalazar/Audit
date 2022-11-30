2f43bf50a9b190ba0ef8d6625b9cf028b4f070b1
Dannicah Salazar <60679947+nicasalazar@users.noreply.github.com>
2022-11-30 09:11:20 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,121 @@
+import connexion 
+from connexion import NoContent 
+import datetime, os, json, yaml, random, uuid, requests, logging, logging.config
+from json.decoder import JSONDecodeError
+from pykafka import KafkaClient
+from flask_cors import CORS, cross_origin
+
+import os
+
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+
+# with open('app_conf.yml', 'r') as f:
+#     app_config = yaml.safe_load(f.read())
+
+# with open('log_conf.yml', 'r') as f:
+#     log_config = yaml.safe_load(f.read())
+#     logging.config.dictConfig(log_config)
+
+# logger = logging.getLogger('basicLogger')
+
+def get_employee_clockin_reading(index): 
+    """ Get employee clockin reading in history """ 
+    hostname = "%s:%d" % (app_config["events"]["hostname"],  
+                          app_config["events"]["port"]) 
+    client = KafkaClient(hosts=hostname) 
+    topic = client.topics[str.encode(app_config["events"]["topic"])] 
+ 
+    # Here we reset the offset on start so that we retrieve 
+    # messages at the beginning of the message queue.  
+    # To prevent the for loop from blocking, we set the timeout to 
+    # 100ms. There is a risk that this loop never stops if the 
+    # index is large and messages are constantly being received! 
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,  
+                                         consumer_timeout_ms=1000) 
+ 
+    logger.info("Retrieving employee clockin at index %d" % index) 
+    try:
+        ci_events = []
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+
+            if msg["type"] == "ci":
+                ci_events.append(msg)
+
+        event = ci_events[index]
+        return event, 200
+    except: 
+        logger.error("No more messages found") 
+     
+    logger.error("Could not find employee-clockin at index %d" % index) 
+    return { "message": "Not Found"}, 404
+
+
+def get_employee_clockout_reading(index): 
+    """ Get employee clockout reading in history """ 
+    hostname = "%s:%d" % (app_config["events"]["hostname"],  
+                          app_config["events"]["port"]) 
+    client = KafkaClient(hosts=hostname) 
+    topic = client.topics[str.encode(app_config["events"]["topic"])] 
+ 
+    # Here we reset the offset on start so that we retrieve 
+    # messages at the beginning of the message queue.  
+    # To prevent the for loop from blocking, we set the timeout to 
+    # 100ms. There is a risk that this loop never stops if the 
+    # index is large and messages are constantly being received! 
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,  
+                                         consumer_timeout_ms=1000) 
+ 
+    logger.info("Retrieving employee clock-out at index %d" % index) 
+    try:
+        co_events = []
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+
+            if msg["type"] == "co":
+                co_events.append(msg)
+
+        event = co_events[index]
+        return event, 200
+    except: 
+        logger.error("No more messages found") 
+     
+    logger.error("Could not find employee clock-out out at index %d" % index) 
+    print()
+    return { "message": "Not Found"}, 404
+
+def health():
+    """ returns health of processing service """
+    return NoContent, 200
+
+app = connexion.FlaskApp(__name__, specification_dir="")
+app.add_api("openapi.yaml", base_path="/audit", strict_validation=True, validate_responses=True)
+
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
+
+if __name__ == "__main__": 
+    app.run(port=8110, use_reloader=False)
